122a762eafcc32e1848079d7beb44325b714a41d
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-04-28 12:50:05 +0600

    init

---
+++ src/main/java/com/example/bulkEmailArchitecture/BulkEmailArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.bulkEmailArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkEmailArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkEmailArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bulkEmailArchitecture/BulkEmailArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bulkEmailArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkEmailArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
